2ef94c7b66011faea3f766749ab5a40fccb46d76
ADRIJA DEY (RA2411003010902) <ad9474@srmist.edu.in>
2025-10-22 17:01:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4130048149-edac7",
+  "appId": "1:315477404307:web:7e4e36da202b2b8c8abb66",
+  "apiKey": "AIzaSyAqKmAzNFtGWkqODtxG-sQncXqVb4-UWTM",
+  "authDomain": "studio-4130048149-edac7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "315477404307"
+};
